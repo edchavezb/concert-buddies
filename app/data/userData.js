dc921676d1f35f0d4b715f31b43f48ef28a808b2
Magpie <edchavez821@gmail.com>
2019-06-06 12:41:34 -0500

    First version random questions

---
+++ app/data/userData.js
@@ -0,0 +1,22 @@
+var usersArray = [
+    {
+        name: "Lenny",
+        photo: "https://i.imgur.com/ujx1607.jpg?1",
+        city: "Paris",
+        scores: ["1", "2", "3", "4"]
+    },
+    {
+        name: "Edgar",
+        photo: "https://i.imgur.com/ItL4Irj.png?2",
+        city: "CDMX",
+        scores: ["4", "3", "2", "1"]
+    },
+    {
+        name: "Alexis",
+        photo: "https://i.imgur.com/ujx1607.jpg?1",
+        city: "CDMX",
+        scores: ["4", "3", "2", "1"]
+    }
+  ];
+  
+  module.exports = usersArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,43 @@
+
+var userData = require("../data/userData");
+
+module.exports = function(app) {
+
+  app.get("/api/users", function(req, res) {
+    res.json(userData);
+  });
+
+  app.post("/api/users", function(req, res) {
+    var myUser = req.body
+    var myResults = req.body.scores
+    console.log(myResults);
+    var comparisonArray = [];
+    for(var i = 0; i < userData.length; i++){
+      var matchingAnswers = 0;
+      for(var j = 0; j < userData[i].scores.length; j++){
+        if (userData[i].scores[j] === myResults[j]){
+          matchingAnswers++;
+        }
+      }
+      comparisonArray.push(matchingAnswers);
+    }
+    var biggestMatch = Math.max.apply(null, comparisonArray);
+    console.log(comparisonArray, biggestMatch);
+    var matchesArray = [];
+    for (var m = 0; m < comparisonArray.length; m++){
+      if(comparisonArray[m] === biggestMatch){
+        console.log(true);
+        matchesArray.push(userData[m]);
+      }
+    }
+    userData.push(myUser);
+    console.log(matchesArray);
+    res.json(matchesArray);
+  });
+
+  app.post("/api/clear", function(req, res) {
+    tableData.length = 0;
+    waitListData.length = 0;
+    res.json({ ok: true });
+  });
+};
